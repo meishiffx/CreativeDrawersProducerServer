3f56e8ee8ba48ef9c77120c57d63655584933137
meishiffx <57701493+meishiffx@users.noreply.github.com>
2024-05-19 15:08:50 +0800

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Forge requires a configured set of both JVM and program arguments.
-REM Add custom JVM arguments to the user_jvm_args.txt
-REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
-REM  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.2.10/unix_args.txt %*
-pause
